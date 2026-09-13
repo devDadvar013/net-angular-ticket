87caca0b988c897648a3944fa489e320f726ec49
unknown <mahdi.dadvar013@gmail.com>
2026-09-13 18:31:02 +0430

    add url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Set your production API base URL here, e.g. "https://api.example.com"
-  apiBaseUrl: "https://net-flight-tickets-api.onrender.com/api",
+  apiBaseUrl: "https://net-flight-tickets-api.onrender.com/api/v1",
 };
